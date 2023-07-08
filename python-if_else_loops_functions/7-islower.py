94e8b70230de3e601444c983fbb706f81bbdccde
Gihozo Innocente <i.gihozo@alustudent.com>
2023-07-08 11:16:11 -0700

    lower and upper case

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-islower = _import_('7-islower').islower
-
-print("a is {}".format("lower" if islower("a") else "upper"))
-print("H is {}".format("lower" if islower("H") else "upper"))
-print("A is {}".format("lower" if islower("A") else "upper"))
-print("3 is {}".format("lower" if islower("3") else "upper"))
-print("g is {}".format("lower" if islower("g") else "upper"))
+def islower(c):
+    if ord('a') <= ord(c) <= ord('z'):
+        return True
+    else:
+        return False
