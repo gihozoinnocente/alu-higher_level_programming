bad499f5fbed58ca77bfad6d33b5e6e76723a064
Gihozo Innocente <i.gihozo@alustudent.com>
2023-07-08 13:00:07 -0700

    uppercase

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
+def uppercase(str):
+    uppercase_str = ""
+    for char in str:
+        uppercase_char = chr(
+            ord(char) - 32) if 97 <= ord(char) <= 122 else char
+        uppercase_str += uppercase_char
 
+    print("{}\n{}".format(uppercase_str, ""))
