26158f9eb3d60120efd1ddbddf387f54a25190d9
Gihozo Innocente <i.gihozo@alustudent.com>
2023-07-08 12:39:57 -0700

    uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-def uppercase(str):
-    for char in str:
+def uppercase(s):
+    for char in s:
         print(chr(ord(char) - 32), end='')
-        print()
+    print()
+
+
 uppercase("best")
 uppercase("Best School 98 Battery street")
