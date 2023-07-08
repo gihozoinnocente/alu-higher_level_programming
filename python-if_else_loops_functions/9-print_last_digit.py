b8e355211032ab1174953d348db16a89582cd823
Gihozo Innocente <i.gihozo@alustudent.com>
2023-07-08 13:08:30 -0700

    last digit

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-def uppercase(str):
-    uppercase_str = ""
-    for char in str:
-        uppercase_char = chr(
-            ord(char) - 32) if 97 <= ord(char) <= 122 else char
-        uppercase_str += uppercase_char
-
-    print("{}\n{}".format(uppercase_str, ""))
+def print_last_digit(number):
+    last_digit = abs(number) % 10
+    print(last_digit)
+    return last_digit
