19220bbff3e280d9d893a9a6ebfcc3202dcb299d
Gihozo Innocente <i.gihozo@alustudent.com>
2023-07-08 08:07:15 -0700

    concet

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str1= str.split(", ")[-1]
-print(str1)
+
+print(str.split(", ")[-1])
