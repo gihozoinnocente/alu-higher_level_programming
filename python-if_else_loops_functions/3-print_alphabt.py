848fec0d7cc6e7ea02c960da12958b8862df8a89
Gihozo Innocente <i.gihozo@alustudent.com>
2023-07-08 12:49:08 -0700

    print all letter except q and e

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
-#!/usr/bin/python3for i in range(97, 123):
+#!/usr/bin/python3
+for i in range(97, 123):
     letter = chr(i)
     if letter not in ['q', 'e']:
         print("{}".format(letter), end='')
+
