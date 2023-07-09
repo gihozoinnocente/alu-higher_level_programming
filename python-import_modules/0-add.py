7676d9eae8e9b01e7f6a4f63949308c5905d45e4
Gihozo Innocente <i.gihozo@alustudent.com>
2023-07-09 05:11:43 -0700

    addition

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-def add(a, b):
-    return a + b
+a = 1
+b = 2
 
-def main():
-    a = 1
-    b = 2
+from add_0 import add
 
-    result = add(a, b)
-    print("{} + {} = {}".format(a, b, result))
-
-if __name__ == "__main__":
-    main()
+result = add(a, b)
+print("{} + {} = {}".format(a, b, result))
