517a4b61251a099416ed135f28cdbeae1d613d24
Gihozo Innocente <i.gihozo@alustudent.com>
2023-07-09 06:01:22 -0700

    argument

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import sys
-arguments = sys.argv[1:]
-num_arguments = len(arguments)
+if __name__ == "__main__":
+    import sys
+    arguments = sys.argv[1:]
+    num_arguments = len(arguments)
 print(f"{num_arguments} argument{'s' if num_arguments != 1 else ''}:")
 print(":" if num_arguments == 0 else "")
 for i, arg in enumerate(arguments, start=1):
