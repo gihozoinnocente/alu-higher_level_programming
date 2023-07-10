48e3da58db5ca8703784c69b920e18a5175511b1
Gihozo Innocente <i.gihozo@alustudent.com>
2023-07-10 11:03:35 -0700

    hidden files

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,26 +1,6 @@
 #!/usr/bin/python3
-import dis
-import marshal
-
-
-def print_module_names():
-    with open('\hidden_4.pyc', 'rb') as file:
-        bytecode = file.read()
-
-    code_obj = marshal.loads(bytecode[8:])
-
-    instructions = dis.get_instructions(code_obj)
-    names = set()
-
-    for instr in instructions:
-        if instr.opname == 'LOAD_GLOBAL':
-            name = instr.argval
-            if not name.startswith('__'):
-                names.add(name)
-
-    for name in sorted(names):
-        print(name)
-
-
-if __name__ == '__main__':
-    print_module_names()
+if __name__ == "__main__":
+    import hidden_4
+    for i in dir(hidden_4):
+        if i.startswith("__") is False:
+            print(i)
