dc38329360bafedc1c2b06a79c8a0e37b45931e7
Gihozo Innocente <i.gihozo@alustudent.com>
2023-07-10 10:33:51 -0700

    variable load

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 from variable_load_5 import a
 import sys
-a.__name__
 if __name__ == "__main__":
     sys.path.append('/alu-higher_level_programming/python-import_modules/variable_load_5.py')
 print(a)
