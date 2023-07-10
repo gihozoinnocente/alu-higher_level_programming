a170eb727f61b89943a9e118093322c1bd8d6da4
Gihozo Innocente <i.gihozo@alustudent.com>
2023-07-10 10:26:54 -0700

    variable load

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 from variable_load_5 import a
 import sys
-
-sys.path.append('D:\Raf\SEIC\Baby\variable_load_5.py')
-
-
+a.__name__
+sys.__name__
+if __name__ == "__main__":
+    sys.path.append('D:\Raf\SEIC\Baby\variable_load_5.py')
 print(a)
