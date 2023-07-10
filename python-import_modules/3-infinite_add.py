a4659d86e6192fa7290e8fb8717a4210333ad047
Gihozo Innocente <i.gihozo@alustudent.com>
2023-07-10 10:15:05 -0700

    infinite add

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 import sys
 
+
 def add_arguments(*args):
     total = sum(int(arg) for arg in args)
     print(total)
+
 
 if __name__ == "__main__":
     arguments = sys.argv[1:]
