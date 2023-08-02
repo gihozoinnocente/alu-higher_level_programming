092b1bfa63e05bc6b503f0cfd6f45b753c96074f
Gihozo Innocente <i.gihozo@alustudent.com>
2023-08-02 08:03:35 -0700

    No genre

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- Genre ID for all shows
+
+SELECT title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON id=tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY title, tv_show_genres.genre_id;
