41704e0cda7942288e9ea4e2df3255899b0e9c5d
Gihozo Innocente <i.gihozo@alustudent.com>
2023-08-03 13:14:59 -0700

    body size

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
-
 # Check if URL is provided as an argument
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-url=$1
-
-# Send request and store the response in a variable
-response=$(curl -s -w "%{size_download}" -o /dev/null "$url")
-
-# Display the size of the body in bytes
-echo "Size of the body: $response bytes"
+#!/bin/bash
+if [ $# -ne 1 ]; then echo "Usage: $0 <URL>"; exit 1; fi
+url=$1; response=$(curl -s -w "%{size_download}" -o /dev/null "$url"); echo "Size of the body: $response bytes"
 
