0ccaca9aef16d93cfd90f68d49aa6293b2aab448
gihozoinnocente <88516265+gihozoinnocente@users.noreply.github.com>
2023-08-18 21:34:18 +0200

    Update 3-say_my_name.py

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
-say_my_name = __import__('3-say_my_name').say_my_name
+"""
+    Module containing ``say_my_name``` function.
+"""
 
-say_my_name("John", "Smith")
-say_my_name("Walter", "White")
-say_my_name("Bob")
-try:
-    say_my_name(12, "White")
-except Exception as e:
-    print(e)
+
+def say_my_name(first_name, last_name=""):
+    """ Function prints `first_name` and `last_name`
+
+    Args:
+        first_name (string): First name.
+        last_name (string): Last name.
+    """
+
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {:s} {:s}".format(first_name, last_name))
