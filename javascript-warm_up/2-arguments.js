85ce8236db476cd95c2e47fc6fc71e9169901c01
gihozoinnocente <i.gihozo@alustudent.com>
2023-09-27 12:31:04 -0700

    arguments bug fix

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const argsLength = process.argv.length
+const argsLength = process.argv.length;
 
-if ( argsLenght === 2 ){
-	console.log( 'No argument');
-} if else ( argsLength === 3) {
-	console.log( 'Argument found');
+if ( argsLength === 2 ){
+	console.log( 'No argument' );
+} if else ( argsLength === 3 ) {
+	console.log( 'Argument found' );
 } else {
-	console.log ( 'Arguments found');
+	console.log ( 'Arguments found' );
 }
