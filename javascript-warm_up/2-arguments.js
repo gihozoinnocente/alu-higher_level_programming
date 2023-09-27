c09ccc3be5f365b6b9ea1e8a341f1a590b3c9742
gihozoinnocente <i.gihozo@alustudent.com>
2023-09-27 11:29:58 -0700

    first constant,languages,arguments

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const arguments = process.argv.length
+
+if ( arguments === 0 ){
+	console.log( 'No argument' )
+} ifelse ( arguments === 1) {
+	console.log( 'Argument found' )
+} else {
+	console.log ( 'Arguments found')
+}
