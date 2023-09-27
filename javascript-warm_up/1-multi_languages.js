f7b0f1b02891742454cf1380d39dba260263d946
gihozoinnocente <i.gihozo@alustudent.com>
2023-09-27 11:49:36 -0700

    semistandard fix

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -2,8 +2,8 @@
 
 const firstLine = 'C is fun';
 const secondLine = 'Python is cool';
-const thirdLine= 'JavaScript is amazing';
+const thirdLine = 'JavaScript is amazing';
 
-console.log(firstLine)
-console.log(secondLine)
-console.log(thirdLine)
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdLine);
