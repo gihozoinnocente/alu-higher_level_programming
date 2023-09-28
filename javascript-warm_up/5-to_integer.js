253d8653e97596d120f19781c758443682cb4ba5
gihozoinnocente <i.gihozo@alustudent.com>
2023-09-27 23:27:19 -0700

    integer

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const firstArg = process.argv[2];
+const isNumber = parseInt(firstArg);
+
+if (!isNumber){
+	console.log("Not a number")
+} else {
+	console.log(`My number: ${isNumber}`)
+}
