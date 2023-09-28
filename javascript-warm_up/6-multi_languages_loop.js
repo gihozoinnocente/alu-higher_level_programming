4ddaf09a1f74d8d61f8eb5e2bc507ff77cb21b9c
gihozoinnocente <i.gihozo@alustudent.com>
2023-09-28 05:25:17 -0700

    multi language loop bug fix

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
-const n = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const n =['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
 for (let i=0; i<n.length; i++) {
         console.log(n[i]);
 }
