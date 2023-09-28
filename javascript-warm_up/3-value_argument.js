a1583e7ea7d73fbbf4da7a8559bf5f46a4705615
gihozoinnocente <i.gihozo@alustudent.com>
2023-09-27 22:49:43 -0700

    semi-standard validation fix

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -2,8 +2,8 @@
 
 const firstArgument = process.argv[2];
 
-if ( firstArgument ) {
-	console.log( firstArgument );
+if (firstArgument) {
+	console.log(firstArgument);
 } else {
 console.log('No argument');
 };
