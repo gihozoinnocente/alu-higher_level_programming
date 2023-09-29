aaab2a91c07113281cd04b66957b38bc0d2bd2a8
gihozoinnocente <88516265+gihozoinnocente@users.noreply.github.com>
2023-09-29 12:26:15 +0200

    Update 2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,16 +1,10 @@
 #!/usr/bin/node
 
 const argsLength = process.argv.length;
-
 if (argsLength === 2) {
   console.log('No argument');
 } else if (argsLength === 3) {
   console.log('Argument found');
 } else {
-<<<<<<< HEAD
-  console.log('Arguments found');
-}
-=======
 	console.log ('Arguments found');
 };
->>>>>>> 559d2eb9a73042f6ba8d9ab6bf2eb2e3c1161012
