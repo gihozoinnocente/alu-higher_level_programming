fa1c741e63279be339630f55f4e9e8cf4773891e
gihozoinnocente <i.gihozo@alustudent.com>
2023-09-29 02:20:18 -0700

    addition

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const n = process.argv[2];
+const m = process.argv[3];
+
+const a = parseInt(n);
+const b = parseInt(m);
+
+function addition(a,b){
+	return(a+b);
+};
+console.log(addition(a,b));
