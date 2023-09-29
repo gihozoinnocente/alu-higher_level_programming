eb5e9f1024aed292b34426f19710b5483fc21d10
gihozoinnocente <88516265+gihozoinnocente@users.noreply.github.com>
2023-09-29 12:14:35 +0200

    Update 11-second_biggest.js

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,26 +1,21 @@
 #!/usr/bin/node
-
 // Get the command line arguments
 const args = process.argv.slice(2);
-
 // Check if no arguments were passed or only one argument was passed
 if (args.length === 0 || args.length === 1) {
   console.log("0");
 } else {
   // Convert the arguments to an array of integers
   const integers = args.map(arg => parseInt(arg));
-
   // Remove any NaN values and duplicates
   const uniqueIntegers = [...new Set(integers.filter(num => !isNaN(num)))];
-
   // Sort the unique integers in descending order
   uniqueIntegers.sort((a, b) => b - a);
-
   // If there's a second largest integer, print it; otherwise, print 0
   if (uniqueIntegers.length >= 2) {
     console.log(uniqueIntegers[1]);
   } else {
     console.log("0");
-  }
-}
+  };
+};
 
