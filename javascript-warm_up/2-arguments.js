e630160202440cbab3cfdbb2382e1579326dac95
gihozoinnocente <i.gihozo@alustudent.com>
2023-09-28 23:55:57 -0700

    validation check

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,10 +2,10 @@
 
 const argsLength = process.argv.length;
 
-if ( argsLength === 2 ){
-	console.log( 'No argument' );
-} else if ( argsLength === 3 ) {
-	console.log( 'Argument found' );
+if (argsLength === 2){
+	console.log('No argument');
+} else if (argsLength === 3) {
+	console.log('Argument found');
 } else {
-	console.log ( 'Arguments found' );
+	console.log ('Arguments found');
 }
