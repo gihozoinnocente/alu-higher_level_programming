9bbeb79fd26d73755b3b24526b4aa9f0f35aa4f2
gihozoinnocente <88516265+gihozoinnocente@users.noreply.github.com>
2023-09-29 12:22:36 +0200

    Update 10-factorial.js

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-function factorial(n) {
+function factorial (n) {
   if (isNaN(n) || n < 0) {
     return 1; // Factorial of NaN or negative number is 1
   } else if (n === 0 || n === 1) {
@@ -16,7 +16,7 @@ const number = parseInt(arg);
 
 // Check if the argument is a valid integer
 if (isNaN(number) || !Number.isInteger(number)) {
-  console.log("1");
+  console.log('1');
 } else {
   // Calculate the factorial using the recursive function
   const result = factorial(number);
