106ff6c3b2f7d83aae192424b457dc5d22f76b14
gihozoinnocente <88516265+gihozoinnocente@users.noreply.github.com>
2023-09-30 20:39:13 +0200

    Update 2-rectangle.js

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 class Rectangle {
-constructor(w, h){
+  constructor (w, h) {
     if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
       return {}; // Create an empty object and return it
     }
-	this.width = w;
-	this.height = h;
-    }
-
+    this.width = w;
+    this.height = h;
+  }
 }
 module.exports = Rectangle;
