2e326160c55f8ca0fda7aae70feff4050cdac125
gihozoinnocente <i.gihozo@alustudent.com>
2023-09-30 11:36:48 -0700

    rectangle

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+class Rectangle {
+constructor(w, h){
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return {}; // Create an empty object and return it
+    }
+	this.width = w;
+	this.height = h;
+    }
+
+}
+module.exports = Rectangle;
