a7c49711c04d648e1dee19b1892e457f949b3783
gihozoinnocente <88516265+gihozoinnocente@users.noreply.github.com>
2023-09-30 18:00:06 +0200

    Update 9-add.js

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -6,7 +6,7 @@ const m = process.argv[3];
 const a = parseInt(n);
 const b = parseInt(m);
 
-function addition(a, b) {
-  return(a + b);
+function addition (a, b) {
+  return (a + b);
 }
 console.log(addition(a, b));
