4c43a7e92ae95fe0837b58a53b6324de3f4d045d
gihozoinnocente <88516265+gihozoinnocente@users.noreply.github.com>
2023-10-01 07:58:12 +0200

    Update 4-rectangle.js

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node#!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
       return {}; // Create an empty object if conditions are not met
     }
@@ -9,27 +9,28 @@ class Rectangle {
     this.height = h;
   }
 
-  print() {
+  print () {
     if (this.width <= 0 || this.height <= 0) {
       return; // If width or height is zero or negative, do nothing
     }
 
     for (let i = 0; i < this.height; i++) {
-      let row = "";
+      let row = '';
       for (let j = 0; j < this.width; j++) {
-        row += "X";
+        row += 'X';
       }
       console.log(row);
     }
   }
-  rotate() {
+  
+  rotate () {
     // Exchange the width and height of the rectangle
     const temp = this.width;
     this.width = this.height;
     this.height = temp;
   }
 
-  double() {
+  double () {
     // Double the width and height of the rectangle
     this.width *= 2;
     this.height *= 2;
