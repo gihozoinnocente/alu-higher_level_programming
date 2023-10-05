0e0a2666c20a95aa81c76ad814ca3e4ba0c0a82f
gihozoinnocente <i.gihozo@alustudent.com>
2023-10-05 12:25:18 -0700

    write me

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+// Check if the file path and string arguments are provided
+if (process.argv.length < 4) {
+  console.error('Usage: node write_file.js <file_path> <string_to_write>');
+  process.exit(1);
+}
+
+// Get the file path and string to write from the command line arguments
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
+
+// Write the string to the file in utf-8 encoding
+fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
+  if (err) {
+    console.error('Error writing to the file:', err);
+  } else {
+    console.log(`${stringToWrite}`);
+  }
+});
